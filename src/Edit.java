4953b4e0a728bdc9cdb88ba4f152f5537d21776f
muzakker <muzakkerh@gmail.com>
2021-02-11 01:18:38 +0600

    Update Edit.java

--- src/Edit.java
+++ src/Edit.java
@@ -27,62 +27,9 @@ public class Edit extends JFrame{
         // this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		/*
-		super("Home");
-		this.setLocation(50, 50);
-		this.setSize(600, 600);
-		//this.setVisible(true);
-		this.setLayout(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		upperPanel = new JPanel(new FlowLayout());
-		this.upperPanel.setBorder(new LineBorder(Color.black, 1));
-		this.upperPanel.setBounds(5, 5, 570, 40);
-		add(this.upperPanel);
-		*/
-
-		/*
-		upperPanel = new JPanel(new FlowLayout());
-		this.upperPanel.setBorder(new LineBorder(Color.black, 1));
-		this.upperPanel.setBounds(5, 5, 570, 40);
-		add(this.upperPanel);
-
-		 */
-
-		/*
-		lowerPanel = new JPanel(new FlowLayout());
-		this.lowerPanel.setBorder(new LineBorder(Color.black, 1));
-		this.lowerPanel.setBounds(5, 50, 570, 40);
-		add(this.lowerPanel);
-
-		 */
 
         lowerPanel = new JPanel(new GridBagLayout());
-        // this.lowerPanel.GridBagConstraints gbc = new GridBagConstraints();
-        // this.lowerPanel.setLayout(grid);
         GridBagConstraints gbc = new GridBagConstraints();
-        // this.lowerPanel.setLayout(new GridBagLayout());
-        // this.lowerPanel.
-
-        //this.lowerPanel.setBorder(new LineBorder(Color.black, 1));
-        //this.lowerPanel.setBounds(5, 5, 570, 270);
-
-
-		/*
-		tfOne = new JTextField(10);
-		this.lowerPanel.add(tfOne);
-
-		 */
-
-		/*
-		get value of 4
-		int row = tblUsers.getSelectedRow();
-        int id = (int) tblUsers.getValueAt(row, 0);
-        String stOne   = (String) tblUsers.getValueAt(row, 1);
-        String stTwo   = (String) tblUsers.getValueAt(row, 2);
-        String stThree = (String) tblUsers.getValueAt(row, 3);
-
-		 */
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -139,19 +86,6 @@ public class Edit extends JFrame{
         gbc.insets = new Insets(0,0,0,0);
         lowerPanel.add(lblFour, gbc);
 
-		/*
-		userType = new JComboBox();
-		DefaultComboBoxModel userModel = new DefaultComboBoxModel();
-		userModel.addElement("doctor");
-		userModel.addElement("receptionist");
-		// userModel.addElement("patient");
-		userType.setModel(userModel);
-		gbc.gridx = 1;
-		gbc.gridy = 3;
-		gbc.insets = new Insets(5,5,5,5);
-		lowerPanel.add(userType,gbc);
-		 */
-
 
         String stUserType[]={"empty", "doctor", "receptionist"};
         userType = new JComboBox(stUserType);
@@ -177,16 +111,8 @@ public class Edit extends JFrame{
                 String stUserTypeData = (String) userType.getItemAt(userType.getSelectedIndex());
                 String sql = "UPDATE `user` SET `id`='"+tfOne.getText()+"',`name`='"+tfTwo.getText()+"',`password`='"+tfThree.getText()+"',`type`='"+stUserTypeData+"' WHERE `id` = '"+intOne+"'";
 
-                // "INSERT INTO user VALUES ("+tfOne.getText()+",'"+tfTwo.getText()+"','"+tfThree.getText()+"','"+stUserTypeData+"')";
-                // String sql = "INSERT INTO user VALUES ("+tfOne.getText()+",'"+tfTwo.getText()+"','"+tfThree.getText()+"','"+stUserTypeData+"')";
-                // one ' there
-                // String sql = "INSERT INTO user VALUES ("+tfOne.getText()+",'"+tfTwo.getText()+"','"+tfThree.getText()+"','"+stUserTypeData+"')";
-                // cb.getItemAt(cb.getSelectedIndex())
-                // String sql = "INSERT INTO user VALUES ("+t1.getText()+",'"+t2.getText()+"','"+t3.getText()+"','"+t4.getText()+"')";
                 DataAccess d = new DataAccess();
                 d.update(sql);
-                // AdminHome.populateTable();
-                // Home.populateTable();
 
                 if(LoginGui.typeLogIn.equals("admin")) {
                     AdminHome.populateTable();
@@ -202,47 +128,9 @@ public class Edit extends JFrame{
 
 
 
-        // add(this.lowerPanel);
-
-        // this.AddUpperComponent();
-        // this.AddLowerComponent();
-
     }
 
     private void AddUpperComponent() {
-
-		/*
-		t1 = new JTextField(10);
-		add(t1);
-		t2 = new JTextField(10);
-		add(t2);
-		t3 = new JTextField(10);
-		add(t3);
-
-		 */
-
-		/*
-		lblOne = new JLabel("ID");
-		this.upperPanel.add(lblOne);
-
-		tfOne = new JTextField(10);
-		this.upperPanel.add(tfOne);
-		 */
-
-
-		/*
-		t4 = new JTextField(10);
-		add(t4);
-		 */
-
-		/*
-		userType = new JComboBox();
-		DefaultComboBoxModel userModel = new DefaultComboBoxModel();
-		userModel.addElement("doctor");
-		userModel.addElement("receptionist");
-		// userModel.addElement("patient");
-		userType.setModel(userModel);
-		 */
 
 		/*
 		String userType[]={"doctor", "receptionist"};
@@ -284,25 +172,4 @@ public class Edit extends JFrame{
 
     }
 
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-String sql = "INSERT INTO user VALUES ("+t1.getText()+",'"+t2.getText()+"','"+t3.getText()+"','"+t4.getText()+"')";
-DataAccess d = new DataAccess();
-d.insert(sql);
-*/
